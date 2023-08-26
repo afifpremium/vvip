1150b408e5bad69e6327d44afd224bddcb3c9d80
afifpremium <143248327+afifpremium@users.noreply.github.com>
2023-08-26 14:10:37 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -288,45 +288,46 @@ netfilter-persistent reload
 
 # download script
 cd /usr/bin
-# menu
-wget -O menu "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/menu.sh"
-wget -O m-vmess "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-vmess.sh"
-wget -O m-vless "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-vless.sh"
-wget -O running "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/clearcache.sh"
-wget -O m-ssws "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-ssws.sh"
-wget -O m-trojan "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-trojan.sh"
-wget -O marzban "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/marzban.sh"
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/menu.sh"
+wget -O m-vmess "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/m-vmess.sh"
+wget -O m-vless "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/m-vless.sh"
+wget -O running "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/running.sh"
+wget -O clearcache "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/clearcache.sh"
+wget -O m-ssws "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/m-ssws.sh"
+wget -O m-trojan "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/m-trojan.sh"
+
 # menu ssh ovpn
-wget -O m-sshovpn "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-sshovpn.sh"
-wget -O usernew "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/renew.sh"
-wget -O hapus "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/hapus.sh"
-wget -O cek "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/cek.sh"
-wget -O member "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/delete.sh"
-wget -O autokill "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/tendang.sh"
-wget -O sshws "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/sshws.sh"
+wget -O m-sshovpn "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/m-sshovpn.sh"
+wget -O usernew "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/delete.sh"
+wget -O autokill "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/tendang.sh"
+wget -O sshws "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/sshws.sh"
 
 # menu system
-wget -O m-system "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-system.sh"
-wget -O m-domain "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-domain.sh"
-wget -O add-host "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/add-host.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/xray/certv2ray.sh"
-wget -O speedtest "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/speedtest_cli.py"
-wget -O auto-reboot "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/auto-reboot.sh"
-wget -O restart "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/restart.sh"
-wget -O bw "https://raw.githubusercontent.com/rizkycky/rvpnstores/master/menu/bw.sh"
-wget -O m-tcp "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/tcp.sh"
-wget -O xp "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/xp.sh"
-wget -O sshws "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/ssh/sshws.sh"
-wget -O m-dns "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/menu/m-dns.sh"
+wget -O m-system "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/m-system.sh"
+wget -O m-domain "https://raw.githubusercontent.com/afifpremium/vvipp
+p/master/menu/m-domain.sh"
+wget -O add-host "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/add-host.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/afifpremium/vvip/master/xray/certv2ray.sh"
+wget -O speedtest "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/speedtest_cli.py"
+wget -O auto-reboot "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/auto-reboot.sh"
+wget -O restart "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/restart.sh"
+wget -O bw "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/bw.sh"
+wget -O m-tcp "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/tcp.sh"
+wget -O xp "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/xp.sh"
+wget -O sshws "https://raw.githubusercontent.com/afifpremium/vvip/master/ssh/sshws.sh"
+wget -O m-dns "https://raw.githubusercontent.com/afifpremium/vvip/master/menu/m-dns.sh"
 
 #UPDATE MENU
-wget -O updatemenu "https://raw.githubusercontent.com/rizkyckj/rvpnstores/master/updatemenu.sh"
+wget -O updatemenu "https://raw.githubusercontent.com/afifpremium/vvip/master/updatemenu.sh"
 
 chmod +x menu
 chmod +x m-vmess
