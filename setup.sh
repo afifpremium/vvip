06ebaa8b4e263c3215940dba8d92ad338805f712
afifpremium <143248327+afifpremium@users.noreply.github.com>
2023-08-26 15:19:06 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
-CDNF="https://raw.githubusercontent.com/rizkyckj/rvpnstores/master"
+CDNF="https://raw.githubusercontent.com/afifpremium/vvip/master"
 ISP=$(wget -qO- ipinfo.io/org)
 CITY=$(curl -s ipinfo.io/city)
 TIME=$(date +'%Y-%m-%d %H:%M:%S')
