c3917e518618b014125f054588897e0156a7b810
afifpremium <143248327+afifpremium@users.noreply.github.com>
2023-09-06 02:31:41 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -192,6 +192,7 @@ echo -e "${COLOR1}    STUNNEL ${NC}: $ressh" "${COLOR1} DROPBEAR ${NC}: $resdbr"
 
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 echo -e "   ${WH}SSHWS ${WH}[${COLOR1}${ssh1}${WH}]  ${WH}VLESS ${WH}[${COLOR1}${vlx}${WH}]  ${WH}VMESS ${WH}[${COLOR1}${vms}${WH}]
+
 echo -e "$COLOR1└───────────────────────────────────────────────────┘${NC}"
 
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
