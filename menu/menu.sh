b185c28869aeeab7f5112499f3a1f64396bec4c8
afifpremium <143248327+afifpremium@users.noreply.github.com>
2023-09-06 02:43:04 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -190,7 +190,7 @@ echo -e "${COLOR1}└───────────────────�
 echo -e "${COLOR1}SSH ${NC}: $ressh"" ${COLOR1} NGINX ${NC}: $resngx"" ${COLOR1}  XRAY ${NC}: $resv2r"" ${COLOR1} TROJAN ${NC}: $resv2r"
 echo -e "${COLOR1}    STUNNEL ${NC}: $ressh" "${COLOR1} DROPBEAR ${NC}: $resdbr" "${COLOR1} UDP-CUSTOM ${NC}: $resv2r" 
 
-echo -e "   ${WH}SSHWS ${WH}[${COLOR1}${ssh1}${WH}]  ${WH}VLESS ${WH}[${COLOR1}${vlx}${WH}]  ${WH}VMESS ${WH}[${COLOR1}${vms}${WH}]
+echo -e " ${WH}SSHWS ${WH}[${COLOR1}${ssh1}${WH}]  ${WH}VLESS ${WH}[${COLOR1}${vlx}${WH}]  ${WH}VMESS ${WH}[${COLOR1}${vms}${WH}]
 
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 echo -e " ${WH}[${COLOR1}01${WH}]${NC} ${y}• ${WH}SSHWS         ${WH}[${COLOR1}07${WH}]${NC} ${Lred}• ${WH}SYSTEM  ${WH}[${COLOR1}ᴍᴇɴᴜ${WH}]  $COLOR1 $NC"
